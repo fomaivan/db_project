c370d25103da7d68a2453052421ecf6b384dfb82
fomaivan <fomin.id@phystech.edu>
2023-04-14 20:56:04 +0300

    Add movie_ticket_inserts.sql

---
+++ scripts/movie_ticket_inserts.sql
@@ -0,0 +1,97 @@
+INSERT INTO mt.employer VALUES (1, 'Ivan', 89854325423, 'ivan@gmail.com', 'Pionersckay 8', '1234 5432 6545 8796', '2023-01-02 12:02:05');
+INSERT INTO mt.employer VALUES (2, 'Petr', 89345323583, 'patr.isefds@gmail.com', 'Pramaya 123', '6785 5432 6545 5237', '2023-01-03 12:02:53');
+INSERT INTO mt.employer VALUES (3, 'Masha', 89857283742, 'masha432@gmail.com', 'Glavnaya 42', '6543 5432 6545 5327', '2023-01-03 12:06:05');
+INSERT INTO mt.employer VALUES (4, 'Sofia', 89867489345, 'sofia@gmail.com', 'Sedmaya 3', '1234 3452 6545 8796', '2023-01-02 12:04:53');
+INSERT INTO mt.employer VALUES (5, 'Arseny', 89965839453, 'arsene7ka@gmail.com', 'Kreml 28', '6542 7653 7654 8796', '2023-01-04 12:04:05');
+INSERT INTO mt.employer VALUES (6, 'Andrey', 89823745273, 'a4ndrey@gmail.com', 'Sedmaya 10', '1234 5432 3453 8796', '2023-01-05 12:02:05');
+INSERT INTO mt.employer VALUES (7, 'John', 89854325542, 'john5354@gmail.com', 'arabskaya 153', '1234 3455 6545 8796', '2023-01-06 12:02:05');
+INSERT INTO mt.employer VALUES (8, 'Ivan', 89854659254, 'ivan_fads@gmail.com', 'Druzhby 8', '1234 5432 6654 5327', '2023-01-07 14:15:05');
+INSERT INTO mt.employer VALUES (9, 'Artem', 89234578492, 'artem@gmail.com', 'Shkolnaya 54', '1234 5432 4563 5326', '2023-01-07 16:19:05');
+INSERT INTO mt.employer VALUES (10, 'Ira', 89854367384, 'ira@gmail.com', 'Kutuzovsky 64', '1234 5432 3452 9844', '2023-01-07 18:20:05');
+INSERT INTO mt.employer VALUES (11, 'Nikita', 89854674095, 'ivan_777@gmail.com', 'Frunzenskaya 53', '1234 5332 6545 5327', '2023-01-07 12:22:05');
+INSERT INTO mt.employer VALUES (12, 'Karlos', 89865789345, 'karlos@yandex.ru', 'Tretyakovsky Proyezd 12', '5423 5432 6545 8796', '2023-01-07 12:23:05');
+INSERT INTO mt.employer VALUES (13, 'StepanM', 89856758394, 'makar@gmail.com', 'Znamenskaya 35', '6327 5432 5432 8796', '2023-01-07 12:43:05');
+INSERT INTO mt.employer VALUES (14, 'KarlosK', 89856238547, 'KarlosK@gmail.com', 'Kutuzovsky 32', '6327 5432 5432 8796', '2023-01-08 12:43:05');
+INSERT INTO mt.employer VALUES (15, 'Liam', 898561985747, 'Liam@gmail.com', 'Sedmaya 15', '7548 5432 5423 8796', '2023-01-08 12:49:00');
+INSERT INTO mt.employer VALUES (16, 'Noah', 89856309867, 'Noah@gmail.com', 'Kutuzovsky 65', '6327 6532 5432 8796', '2023-01-08 12:43:05');
+INSERT INTO mt.employer VALUES (17, 'Oliver', 89856309687, 'Oliver@gmail.com', 'Sedmaya 34', '7654 5432 2345 8796', '2023-01-08 12:45:05');
+INSERT INTO mt.employer VALUES (18, 'Elijah', 89856749586, 'Elijah@gmail.com', 'arabskaya 54', '6435 6543 5432 8796', '2023-01-08 12:46:05');
+INSERT INTO mt.employer VALUES (19, 'James', 89856239967, 'James@gmail.com', 'Znamenskaya 62', '6327 3456 5432 5432', '2023-01-08 12:47:05');
+INSERT INTO mt.employer VALUES (20, 'William', 89856205947, 'William@gmail.com', 'Sedmaya 73', '6327 5432 6453 8796', '2023-01-08 14:56:05');
+INSERT INTO mt.employer VALUES (21, 'Benjamin', 89856384967, 'Benjamin@gmail.com', 'Kutuzovsky 34', '6543 5432 5432 8796', '2023-01-09 09:54:05');
+INSERT INTO mt.employer VALUES (22, 'Emma', 89858493567, 'Emma@gmail.com', 'Frunzenskaya 53', '6327 5432 6437 8796', '2023-01-09 09:59:05');
+INSERT INTO mt.employer VALUES (23, 'Charlotte', 89856489547, 'Charlotte@gmail.com', 'Kutuzovsky 32', '6543 7653 5432 3452', '2023-01-09 14:43:05');
+INSERT INTO mt.employer VALUES (24, 'Amelia', 89848593567, 'Amelia@gmail.com', 'Glavnaya 54', '6327 6543 6543 8796', '2023-01-09 15:40:00');
+INSERT INTO mt.employer VALUES (25, 'Ava', 89853869567, 'Ava@gmail.com', 'Kutuzovsky 23', '6327 5432 6437 8796', '2023-01-09 16:25:54');
+INSERT INTO mt.employer VALUES (26, 'Sophia', 89856296757, 'Sophia@gmail.com', 'arabskaya 43', '6327 7537 5432 8796', '2023-01-09 17:20:00');
+INSERT INTO mt.employer VALUES (27, 'Evelyn', 89856475867, 'Evelyn@gmail.com', 'Kutuzovsky 12', '6327 2754 5432 6538', '2023-01-10 18:43:00');
+INSERT INTO mt.employer VALUES (28, 'Theodore', 8985674857, 'Theodore@gmail.com', 'Frunzenskaya 25', '6542 5432 5386 8796', '2023-01-10 19:43:05');
+INSERT INTO mt.employer VALUES (29, 'Henry', 89856737685, 'Henry@gmail.com', 'Glavnaya 64', '7654 5432 5432 8796', '2023-01-10 19:50:00');
+INSERT INTO mt.employer VALUES (30, 'Lucas', 89856767584, 'Lucas@gmail.com', 'Kutuzovsky 35', '7653 5432 6547 8796', '2023-01-19 19:55:00');
+
+
+INSERT INTO mt.movie VALUES (1, 'Волк с Уолл-стрит', 'Драма, Комедия', 'Мартин Скорсезе', 'Леонардо Дикаприо, Марго Робби', '1987 год. Джордан Белфорт становится брокером в успешном инвестиционном банке. Вскоре банк закрывается после внезапного обвала индекса Доу-Джонса...', 120);
+INSERT INTO mt.movie VALUES (2, 'Омерзительная восьмерка', 'Криминал, Драма', 'Квентин Тарантино', 'Сэмюэл Л. Джексон, Курт Расселл, Дженнифер Джейсон Ли', 'США после Гражданской войны. Легендарный охотник за головами Джон Рут по кличке Вешатель конвоирует заключенную. По пути к ним прибиваются еще несколько путешественников...', 168);
+
+
+INSERT INTO mt.cinema VALUES (1, 'Мираж синема', 37.6, 55.7);
+INSERT INTO mt.cinema VALUES (2, 'Октяборьский', 37.65, 55.654);
+
+INSERT INTO mt.hall VALUES (1, 1, 10);
+INSERT INTO mt.hall VALUES (2, 1, 10);
+INSERT INTO mt.hall VALUES (3, 1, 10);
+INSERT INTO mt.hall VALUES (4, 1, 10);
+INSERT INTO mt.hall VALUES (5, 1, 10);
+INSERT INTO mt.hall VALUES (6, 2, 10);
+INSERT INTO mt.hall VALUES (7, 2, 10);
+INSERT INTO mt.hall VALUES (8, 2, 10);
+INSERT INTO mt.hall VALUES (9, 2, 10);
+
+INSERT INTO mt.screening VALUES (1, 1, 1, 1, '2023-04-13 09:00:00');
+INSERT INTO mt.screening VALUES (2, 1, 2, 1, '2023-04-13 13:00:00');
+INSERT INTO mt.screening VALUES (3, 1, 1, 1, '2023-04-13 17:00:00');
+INSERT INTO mt.screening VALUES (4, 1, 1, 1, '2023-04-13 21:00:00');
+INSERT INTO mt.screening VALUES (5, 2, 1, 1, '2023-04-13 14:00:00');
+INSERT INTO mt.screening VALUES (6, 2, 2, 1, '2023-04-13 16:00:00');
+INSERT INTO mt.screening VALUES (7, 2, 3, 1, '2023-04-13 10:00:00');
+INSERT INTO mt.screening VALUES (8, 1, 2, 2, '2023-04-13 20:00:00');
+
+INSERT INTO mt.session_cinema VALUES (1, 1, 1);
+INSERT INTO mt.session_cinema VALUES (2, 2, 1);
+INSERT INTO mt.session_cinema VALUES (3, 3, 1);
+INSERT INTO mt.session_cinema VALUES (4, 4, 1);
+INSERT INTO mt.session_cinema VALUES (5, 5, 2);
+INSERT INTO mt.session_cinema VALUES (6, 6, 2);
+INSERT INTO mt.session_cinema VALUES (7, 7, 2);
+INSERT INTO mt.session_cinema VALUES (8, 8, 2);
+
+INSERT INTO mt.ticket VALUES (1, 5, 1, 6);
+INSERT INTO mt.ticket VALUES (2, 7, 1, 1);
+INSERT INTO mt.ticket VALUES (3, 3, 1, 2);
+INSERT INTO mt.ticket VALUES (4, 25, 1, 3);
+INSERT INTO mt.ticket VALUES (5, 21, 1, 10);
+INSERT INTO mt.ticket VALUES (13, 6, 2, 1);
+INSERT INTO mt.ticket VALUES (6, 17, 3, 3);
+INSERT INTO mt.ticket VALUES (7, 1, 3, 5);
+INSERT INTO mt.ticket VALUES (8, 2, 3, 7);
+INSERT INTO mt.ticket VALUES (9, 20, 3, 1);
+INSERT INTO mt.ticket VALUES (10, 16, 5, 1);
+INSERT INTO mt.ticket VALUES (11, 23, 5, 5);
+INSERT INTO mt.ticket VALUES (12, 24, 5, 2);
+INSERT INTO mt.ticket VALUES (14, 4, 8, 1);
+
+
+INSERT INTO mt.purchase VALUES (1, 1, 1, 400);
+INSERT INTO mt.purchase VALUES (2, 2, 1, 400);
+INSERT INTO mt.purchase VALUES (3, 3, 1, 400);
+INSERT INTO mt.purchase VALUES (4, 4, 1, 400);
+INSERT INTO mt.purchase VALUES (5, 5, 1, 400);
+INSERT INTO mt.purchase VALUES (6, 6, 2, 300);
+INSERT INTO mt.purchase VALUES (7, 7, 3, 300);
+INSERT INTO mt.purchase VALUES (8, 8, 3, 300);
+INSERT INTO mt.purchase VALUES (9, 9, 3, 300);
+INSERT INTO mt.purchase VALUES (10, 10, 5, 450);
+INSERT INTO mt.purchase VALUES (11, 11, 5, 450);
+INSERT INTO mt.purchase VALUES (12, 12, 5, 450);
+INSERT INTO mt.purchase VALUES (13, 13, 2, 500);
+INSERT INTO mt.purchase VALUES (14, 13, 8, 600);
